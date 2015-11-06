8e768fb9b38bc39e3a801465602496923594eb7a
Ludovic Claude <ludovic.claude@laposte.net>
2015-11-07 00:16:08 +0100

    Handle different dir

--- start-db.sh
+++ start-db.sh
@@ -1,8 +1,19 @@
 #!/bin/sh -e
 
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     $( cd -P "$( dirname "$SOURCE" )" )
+     pwd
+}
+
 sudo docker rm --force test-postgres 2> /dev/null | true
 sudo docker run --name test-postgres \
-    -v $(pwd):/tests \
+    -v $(get_script_dir):/tests \
     -e POSTGRES_PASSWORD=test -p 5432:5432 -d postgres:9.4.4
 
 sudo docker exec test-postgres \
