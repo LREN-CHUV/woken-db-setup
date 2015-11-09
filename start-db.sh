b5dbdcf0f57c54e1e5ac3d45e2bdbcff63dea9d1
Ludovic Claude <ludovic.claude54@gmail.com>
2015-11-09 16:14:41 +0100

    Don't expose port to outside, prefer link mecanism

--- start-db.sh
+++ start-db.sh
@@ -15,7 +15,7 @@ get_script_dir () {
 sudo docker rm --force analyticsdb 2> /dev/null | true
 sudo docker run --name analyticsdb \
     -v $(get_script_dir):/tests \
-    -e POSTGRES_PASSWORD=test -p 25432:5432 -d postgres:9.4.4
+    -e POSTGRES_PASSWORD=test -d postgres:9.4.4
 
 sudo docker exec analyticsdb \
     /bin/bash -c 'while ! pg_isready -U postgres ; do sleep 1; done && exec psql -U postgres -f /tests/create.sql'
