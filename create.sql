a6c2fa93b6fb6d076cb389b704a93790245a6be6
Ludovic Claude <ludovic.claude54@gmail.com>
2015-11-10 17:10:45 +0100

    Rename id to job_id

--- create.sql
+++ create.sql
@@ -1,13 +1,13 @@
 
 CREATE TABLE job_result
 (
-  request_id character varying(32) NOT NULL,
+  job_id character varying(32) NOT NULL,
   node character varying(32) NOT NULL,
   timestamp timestamp default now(),
   data text,
   error character varying(256),
 
-  CONSTRAINT pk_job_result PRIMARY KEY (request_id, node)
+  CONSTRAINT pk_job_result PRIMARY KEY (job_id, node)
 )
 WITH (
   OIDS=FALSE
