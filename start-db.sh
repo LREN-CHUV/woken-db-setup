b4a9e884537579e1e7f872f6bab269995e4fd249
Ludovic Claude <ludovic.claude54@gmail.com>
2015-11-11 11:36:25 +0100

    Upgrade Postgres to 9.4.5

--- start-db.sh
+++ start-db.sh
@@ -21,7 +21,7 @@ fi
 $DOCKER rm --force analyticsdb 2> /dev/null | true
 $DOCKER run --name analyticsdb \
     -v $(get_script_dir):/tests \
-    -e POSTGRES_PASSWORD=test -d postgres:9.4.4
+    -e POSTGRES_PASSWORD=test -d postgres:9.4.5
 
 $DOCKER exec analyticsdb \
     /bin/bash -c 'while ! pg_isready -U postgres ; do sleep 1; done && exec psql -U postgres -f /tests/create.sql'
