30aa8f8d7dcb7e002ac63af6f87b836d25aee3b4
Ludovic Claude <ludovic.claude@laposte.net>
2016-01-13 23:54:11 +0100

    Add shape column

--- sql/create.sql
+++ sql/create.sql
@@ -6,6 +6,7 @@ CREATE TABLE job_result
   timestamp timestamp default now(),
   data text,
   error character varying(256),
+  shape character varying(256),
 
   CONSTRAINT pk_job_result PRIMARY KEY (job_id, node)
 )
