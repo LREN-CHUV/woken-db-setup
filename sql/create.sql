8dbafab3f52bf8b9a861a4208ae855b0dceefa7b
Ludovic Claude <ludovic.claude54@gmail.com>
2016-01-22 18:33:16 +0100

    Add function field to track which function produced the result

--- sql/create.sql
+++ sql/create.sql
@@ -7,6 +7,7 @@ CREATE TABLE job_result
   data text,
   error character varying(256),
   shape character varying(256),
+  function character varying(256),
 
   CONSTRAINT pk_job_result PRIMARY KEY (job_id, node)
 )
