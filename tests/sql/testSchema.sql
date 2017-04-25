d364e3b32721c6ac050d8a4aa1cfbd69f1ab9517
Ludovic Claude <ludovic.claude54@gmail.com>
2017-04-25 13:23:12 +0200

    Tests pass

--- tests/sql/testSchema.sql
+++ tests/sql/testSchema.sql
@@ -1,35 +1,12 @@
 BEGIN;
-SELECT plan( 18 );
+SELECT plan( 5 );
 
-SELECT has_table( 'domains' );
-SELECT has_table( 'stuff' );
-SELECT has_table( 'sources' );
-SELECT has_table( 'domain_stuff' );
+SELECT has_table( 'job_result' );
 
-SELECT has_column( 'domains', 'id' );
-SELECT col_is_pk(  'domains', 'id' );
-SELECT has_column( 'domains', 'domain' );
-
-SELECT has_column( 'stuff',   'id' );
-SELECT col_is_pk(  'stuff', 'id' );
-SELECT has_column( 'stuff',   'name' );
-
-SELECT has_column( 'sources', 'id' );
-SELECT col_is_pk(  'sources', 'id' );
-SELECT has_column( 'sources', 'name' );
-
-SELECT has_column( 'domain_stuff', 'domain_id' );
-SELECT has_column( 'domain_stuff', 'source_id' );
-SELECT has_column( 'domain_stuff', 'stuff_id' );
-SELECT col_is_pk(
-    'domain_stuff',
-    ARRAY['domain_id', 'source_id', 'stuff_id']
-);
-
-SELECT can_ok(
-    'insert_stuff',
-    ARRAY[ 'text', 'integer[]', 'integer', 'integer' ]
-);
+SELECT has_column( 'job_result', 'job_id' );
+SELECT has_column( 'job_result', 'node' );
+SELECT col_is_pk(  'job_result', Array['job_id', 'node'] );
+SELECT has_column( 'job_result', 'data' );
 
 SELECT * FROM finish();
 ROLLBACK;
