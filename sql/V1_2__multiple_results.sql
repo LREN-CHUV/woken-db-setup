f7250c36db2bf480fc5fb3544c7aba53b4b15ecd
Ludovic Claude <ludovic.claude54@gmail.com>
2018-07-16 18:30:39 +0200

    Fix upgrade of job_result when there is existing data

--- sql/V1_2__multiple_results.sql
+++ sql/V1_2__multiple_results.sql
@@ -2,12 +2,12 @@ ALTER TABLE job_result
   ADD COLUMN result_name character varying(32) DEFAULT '',
   ADD COLUMN result_title character varying(256),
   ALTER COLUMN shape SET not null,
-  DROP CONSTRAINT pk_job_result,
-  ADD CONSTRAINT pk_job_result PRIMARY KEY (job_id, shape, result_name, node)
+  DROP CONSTRAINT pk_job_result
 ;
 
 UPDATE job_result SET result_name = '';
 
 ALTER TABLE job_result
+  ADD CONSTRAINT pk_job_result PRIMARY KEY (job_id, shape, result_name, node),
   ALTER COLUMN result_name SET not null
 ;
