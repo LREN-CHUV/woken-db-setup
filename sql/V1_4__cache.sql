58d8e8efd68b4aa3e0dc1758637ea3760bf681e4
Ludovic Claude <ludovic.claude54@gmail.com>
2019-02-02 01:10:48 +0100

    Add cache for mining results

--- sql/V1_4__cache.sql
+++ sql/V1_4__cache.sql
@@ -1,13 +1,18 @@
-CREATE TABLE job_cache
+CREATE TABLE "results_cache"
 (
-  job_id character varying(128) NOT NULL,
-  node character varying(32) NOT NULL,
-  timestamp timestamp default now(),
-  json_data jsonb,
-  shape character varying(256),
-  function character varying(256),
+  "job_id" character varying(128) NOT NULL,
+  "node" character varying(32) NOT NULL,
+  "table_name" character varying(128) NOT NULL,
+  "table_contents_hash" character varying(128),
+  "query_where" text NOT NULL,
+  "query" jsonb NOT NULL,
+  "created_at" timestamp default now(),
+  "last_used" timestamp,
+  "data" jsonb,
+  "shape" character varying(256),
+  "function" character varying(256),
 
-  CONSTRAINT pk_job_cache PRIMARY KEY (job_id, node)
+  CONSTRAINT pk_results_cache PRIMARY KEY ("job_id", "node", "table_name", "table_contents_hash", "query_where")
 )
 WITH (
   OIDS=FALSE
