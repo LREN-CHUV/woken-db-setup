e2800d221b31f48bd9c0ca0d0c32fb6790b87f80
Ludovic Claude <ludovic.claude54@gmail.com>
2019-02-03 22:50:22 +0100

    Use full query in the pk of results_cache

--- sql/V1_4__cache.sql
+++ sql/V1_4__cache.sql
@@ -4,7 +4,6 @@ CREATE TABLE "results_cache"
   "node" character varying(32) NOT NULL,
   "table_name" character varying(128) NOT NULL,
   "table_contents_hash" character varying(128),
-  "query_where" text NOT NULL,
   "query" jsonb NOT NULL,
   "created_at" timestamp default now(),
   "last_used" timestamp,
@@ -12,7 +11,7 @@ CREATE TABLE "results_cache"
   "shape" character varying(256),
   "function" character varying(256),
 
-  CONSTRAINT pk_results_cache PRIMARY KEY ("job_id", "node", "table_name", "table_contents_hash", "query_where")
+  CONSTRAINT pk_results_cache PRIMARY KEY ("job_id", "node", "table_name", "table_contents_hash", "query")
 )
 WITH (
   OIDS=FALSE
