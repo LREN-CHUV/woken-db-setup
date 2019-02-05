c33ac7989fdaab6e8e314cf91c220f271b4f52c0
Ludovic Claude <ludovic.claude54@gmail.com>
2019-02-05 10:19:29 +0100

    Remove jobId from results_cache table

--- sql/V1_4__cache.sql
+++ sql/V1_4__cache.sql
@@ -1,6 +1,5 @@
 CREATE TABLE "results_cache"
 (
-  "job_id" character varying(128) NOT NULL,
   "node" character varying(32) NOT NULL,
   "table_name" character varying(128) NOT NULL,
   "table_contents_hash" character varying(128),
@@ -11,7 +10,7 @@ CREATE TABLE "results_cache"
   "shape" character varying(256),
   "function" character varying(256),
 
-  CONSTRAINT pk_results_cache PRIMARY KEY ("job_id", "node", "table_name", "table_contents_hash", "query")
+  CONSTRAINT pk_results_cache PRIMARY KEY ("node", "table_name", "table_contents_hash", "query")
 )
 WITH (
   OIDS=FALSE
